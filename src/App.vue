e8c1acd6d6b8bf1f58dd5286f9f8eca11ac42507
Ricardo <ricarditomontserrat@gmail.com>
2023-11-10 00:34:11 +0100

    I added more templates

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,22 @@ const IMAGES = {
 	meditation: "https://thumbs.dreamstime.com/z/third-eye-concept-meditation-vision-energy-aura-third-eye-concept-meditation-vision-energy-aura-vector-sketch-171184993.jpg",
 	nap: "https://th.bing.com/th/id/R.f84debda0a41492b9fd402bafd118b4e?rik=5VjMzBlvL0vG9Q&pid=ImgRaw&r=0",
 };
+
+// Show approximated time saving for points
 </script>
 
 <template>
-	<ButtonMain :goal="10_000" />
 	<div id="activities">
 		<Activity :image="IMAGES.study" name="Study" :steps="['Select a time', 'Study', 'Complete']" :points="2000" :totalTime="3600" />
 		<Activity :image="IMAGES.devlog" name="Devlog" :steps="['Select a time', 'Devlog', 'Complete']" :points="1000" :totalTime="3600" />
 		<Activity :image="IMAGES.meditation" name="Meditation" :steps="['Select a time', 'Meditate', 'Complete']" :points="500" :totalTime="3600" />
 		<Activity :image="IMAGES.nap" name="Nap" :steps="['Select a time', 'Nap', 'Complete']" :points="700" :totalTime="3600" />
+	</div>
+	<ButtonMain :goal="10_000" />
+	<div id="activities">
+		<Activity name="Shower" :steps="['Do it']" :points="300" :totalTime="3600" />
+		<Activity name="Upgrade Tool" :steps="['Do it']" :points="800" :totalTime="3600" />
+		<Activity name="Create Tool" :steps="['Do it']" :points="900" :totalTime="3600" />
 		<Activity name="Sudden Task" :steps="['???']" points="???" totalTime="???" />
 	</div>
 </template>
